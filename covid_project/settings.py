37c3f6527ee6fd203550f9982a5d3cae9ab406ec
tareqzoubii <tareqzoubi4@gmail.com>
2023-04-18 22:17:18 +0300

    Version 4

--- covid_project/settings.py
+++ covid_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'covid_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sqjlprom',
+        'USER': 'sqjlprom',
+        'PASSWORD': 'zOu_xIGy7DvtLQjGb14xsSMPS8FeozYX',
+        'HOST': 'snuffleupagus.db.elephantsql.com',
+        'PORT': '',
     }
 }
 
